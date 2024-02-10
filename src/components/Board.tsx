5ff3d1b319564dba050067dc0b8dbe7fc205aab6
Joaco <joaco.kam@gmail.com>
2024-02-10 18:03:20 -0300

    Basic static board

---
+++ src/components/Board.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Board.css';
+
+function Board() {
+	return (
+		<div className="board">
+			<div className="cell">
+			.
+			</div>
+			<div className="cell">
+			</div>
+			<div className="cell">
+			</div>
+			<div className="cell">
+			</div>
+			<div className="cell">
+			</div>
+			<div className="cell">
+			</div>
+			<div className="cell">
+			</div>
+			<div className="cell">
+			</div>
+			<div className="cell">
+			</div>
+			<div className="cell">
+			</div>
+			<div className="cell">
+			</div>
+		</div>
+	)
+}
+
+export default Board;
