375aff013c683e71205b96c8f093b683dd6802ed
Joaco <joaco.kam@gmail.com>
2024-02-18 10:31:12 -0300

    Set player posiion

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -4,15 +4,35 @@ import './Board.css';
 function Board() {
 
 	const [players, setPlayers] = useState([
-		{ id: 1, color: 'DeepSkyBlue', position: 379 },
-		{ id: 2, color: 'LawnGreen', position: 379 },
-		{ id: 3, color: 'Violet', position: 379 },
+		{ id: 1, color: 'DeepSkyBlue', position: 379, selected: false },
+		{ id: 2, color: 'LawnGreen', position: 379, selected: false },
+		{ id: 3, color: 'Violet', position: 379, selected: false },
 	]);
 
 	const totalCells = 720;
 	const cellsPerColor = 8;
 
 	const colorRanges = [
+		// Special cells
+	  { color: 'white', startId: 339, endId: 339 },
+	  { color: 'white', startId: 340, endId: 340 },
+	  { color: 'white', startId: 379, endId: 379 },
+	  { color: 'white', startId: 380, endId: 380 },
+	  { color: 'white', startId: 329, endId: 329 },
+	  { color: 'white', startId: 369, endId: 369 },
+	  { color: 'white', startId: 350, endId: 350 },
+	  { color: 'white', startId: 390, endId: 390 },
+
+	  { color: 'black', startId: 160, endId: 160 },
+	  { color: 'black', startId: 14, endId: 14 },
+	  { color: 'black', startId: 26, endId: 26 },
+	  { color: 'black', startId: 520, endId: 520 },
+	  { color: 'black', startId: 694, endId: 694 },
+	  { color: 'black', startId: 706, endId: 706 },
+	  { color: 'black', startId: 199, endId: 199 },
+	  { color: 'black', startId: 559, endId: 559 },
+
+
 	  { color: 'red', startId: 0, endId: 9 },
 	  { color: 'red', startId: 40, endId: 49 },
 	  { color: 'red', startId: 80, endId: 89 },
@@ -63,15 +83,15 @@ function Board() {
 	  { color: 'blue', startId: 640, endId: 649 },
 	  { color: 'blue', startId: 680, endId: 689 },
 
-	  { color: 'white', startId: 370, endId: 379 },
-	  { color: 'white', startId: 410, endId: 419 },
-	  { color: 'white', startId: 450, endId: 459 },
-	  { color: 'white', startId: 490, endId: 499 },
-	  { color: 'white', startId: 530, endId: 539 },
-	  { color: 'white', startId: 570, endId: 579 },
-	  { color: 'white', startId: 610, endId: 619 },
-	  { color: 'white', startId: 650, endId: 659 },
-	  { color: 'white', startId: 690, endId: 699 },
+	  { color: 'lightgray', startId: 370, endId: 379 },
+	  { color: 'lightgray', startId: 410, endId: 419 },
+	  { color: 'lightgray', startId: 450, endId: 459 },
+	  { color: 'lightgray', startId: 490, endId: 499 },
+	  { color: 'lightgray', startId: 530, endId: 539 },
+	  { color: 'lightgray', startId: 570, endId: 579 },
+	  { color: 'lightgray', startId: 610, endId: 619 },
+	  { color: 'lightgray', startId: 650, endId: 659 },
+	  { color: 'lightgray', startId: 690, endId: 699 },
 
 	  { color: 'orange', startId: 380, endId: 389 },
 	  { color: 'orange', startId: 420, endId: 429 },
@@ -114,32 +134,69 @@ function Board() {
 		return players.filter((player) => player.position === cellId);
 	};
 
+	const PlayersButtons = ({ players, onPlayerButtonClick }) => (
+	    <div className="players-buttons">
+	      {players.map(player => (
+	        <button key={player.id} onClick={() => onPlayerButtonClick(player.id)} className={`player-button player-${player.id}`}>
+	          Mover Jugador {player.id}
+	        </button>
+	      ))}
+	    </div>
+  	);
+
+  	const handlePlayerButtonClick = playerId => {
+		const randomRoll = Math.floor(Math.random() * 6) + 1;
+  		alert(`Jugador ${playerId} mueve ${randomRoll} casilleros`);
+
+	  setPlayers(prevPlayers =>
+	    prevPlayers.map(player =>
+	      player.id === playerId
+	        ? { ...player, selected: true } // Toggle el estado de selección del jugador
+	        : { ...player, selected: false }
+	    )
+	  );
+	};
+
+	const handleCellClick = cellId => {
+	  const selectedPlayer = players.find(player => player.selected);
+
+	  if (selectedPlayer) {
+	    setPlayers(prevPlayers =>
+	      prevPlayers.map(player =>
+	        player.id === selectedPlayer.id
+	          ? { ...player, position: cellId }
+	          : player
+	      )
+	    );
+	  }
+	};
+
 	return (
-		<div className="board">
-			{cells.map((cell) => (
-				<div 
-					key={cell.id}
-					className="cell"
-					style={{backgroundColor: cell.color}}
-				>
-					{getPlayersInCell(cell.id).map((player, index, array) => (
-						<div
-							key={player.id}
-							//className="player"
-							className={`player players-in-cell-${getPlayersInCell(cell.id).length} player-number-${index}`}
-							style={{
-								backgroundColor: player.color,
-								// width: `${Math.max(100 - 25 * (array.length - 1), 10)}%`,
-                				// height: `${Math.max(100 - 25 * (array.length - 1), 10)}%`,
-								// left: `${index * (10 * (array.length - 1))}px`,
-								// top: `${index * (10 * (array.length - 1))}px`
-							}}
-						>
-							
-						</div>
-					))}	
-				</div>
-			))}
+		<div>
+			<div className="board">
+				{cells.map((cell) => (
+					<div 
+						key={cell.id}
+						className="cell"
+						style={{backgroundColor: cell.color}}
+						onClick={() => handleCellClick(cell.id)}
+					>
+						{getPlayersInCell(cell.id).map((player, index, array) => (
+							<div
+								key={player.id}
+								//className="player"
+								className={`player players-in-cell-${getPlayersInCell(cell.id).length} player-number-${index}`}
+								style={{
+									backgroundColor: player.color,
+								}}
+							>
+								
+							</div>
+						))}	
+					</div>
+				))}
+			</div>
+			<PlayersButtons players={players} onPlayerButtonClick={handlePlayerButtonClick} />
 		</div>
 	)
 }
